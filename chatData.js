c40d6e75d33dfae3600b611b12a672eb3553c571
Yash-debug489 <yash.kumaranime@gmail.com>
2025-12-27 19:02:09 +0530

    Update and rename chat-data.js to chatData.js

---
+++ chatData.js
@@ -0,0 +1,29 @@
+const CHAT = [
+  // PAGE 1
+  { side: "left",  text: "This year was preety surprising don't u thing?" },
+  { side: "right", text: "Haa yrrr....Haam aasman se gir ke khajur me atak gayee.." },
+  { side: "left",  text: "Hainaa...tu M se XL ho vii ho gaii and vii merii 206 se jyda hadiya ho gaii thii ak wakt pe [Unglii crush huii thii na😂]" },
+  { side: "right", text: "Bakwas mat kar" },
+  { side: "left",  text: "Hehehe...Waise u know why im so happy....Now, i have the best thing i could have had😌." },
+  { side: "right", text: "Mere baree maii baat kr ree hoo naaa" },
+  { side: "left",  text: "Maybe.." },
+  { side: "right", text: "Maybee!!..fir kya haii😒" },
+  { side: "left",  text: "baby👉👈i wanna show u, sabse pehlee at the start of year💞💞...." },
+  { side: "right", text: `click <a href="camera.html">HERE</a>`, html: true },
+
+  // PAGE 2
+  { side: "right", text: "😳😳💕cutee thaa yrr" },
+  { side: "left",  text: "wait, the website is not over yet..." },
+  { side: "right", text: "aggee bhii kuch haii??" },
+  { side: "left",  text: "haa meri profile pe click kar" },
+
+  // PAGE 3
+  { side: "left",  text: "hehe, its again me so i basically ordered u......" },
+  { side: "left",  text: "okayy.. ab post 2 pe jao" },
+
+  // PAGE 4
+  { side: "left",  text: "its over noww..." },
+  { side: "left",  text: "happy new year to u my love, and happy new year to us!!" }
+];
+
+
