2a4ed6c5408ab89c701cbfefcee2269a937117c8
Yash-debug489 <yash.kumaranime@gmail.com>
2025-12-29 13:34:30 +0530

    Update chatData.js

--- js/chatData.js
+++ js/chatData.js
@@ -1,29 +1,29 @@
 const CHAT = [
   // PAGE 1
-  { side: "left",  text: "This year was preety surprising don't u thing?" },
-  { side: "right", text: "🙂🙂..Haa Haam assamn se giree and khajoor me aatke!" },
-  { side: "left",  text: "Hainaa....Peet phool gya iss saal ispe baat karogi..waise i love u tum jaisii bhii hoo..." },
-  { side: "right", text: "Just wait Haam 2026 mai patle honge" },
-  { side: "left",  text: "Waise u know why im so happy....bcz i'm entering 2026 with the best thing ever" },
-  { side: "right", text: "kyaaa" },
-  { side: "left",  text: "Wanna see it?? i don't wanna hype it up but it's the best thing in this entire universe😌" },
-  { side: "right", text: "Assa kyaaa haii?" },
-  { side: "left",  text: "baby👉👈,i want u to be the first person im showing this to at the start of the year!!" },
+  { side: "left",  text: "Yooo..so ur fianlly heree...welcome make urself comfortable.." },
+  { side: "right", text: "Yupp..waisee haii kyaa yahann pee.." },
+  { side: "left",  text: "sabar karoo madam\nu know slowly opening up..build up before climax" },
+  { side: "left", text: "waise u know why i'm so happy??" },
+  { side: "right",  text: "kyuu??" },
+  { side: "left", text: "Im entering 2026 with the best thing ever😌" },
+  { side: "right",  text: "Assa kyaa haii?" },
+  { side: "left", text: "👉👈" },
+  { side: "left",  text: "Wanna see it??" },
   { side: "left", text: `click <a href="camera.html">HERE</a>`, html: true },
 
   // PAGE 2
-  { side: "right", text: "😳😳💕cutee thaa yrr" },
+  { side: "right", text: "😳💕cutee thaa yrr" },
   { side: "left",  text: "wait, website khtm nii huii haii" },
   { side: "right", text: "aggee bhii kuch haii??" },
-  { side: "left",  text: "haa meri profile pic pe click karo " },
+  { side: "left",  text: "haa meri profile pic pe click karo👉👈" },
 
   // PAGE 3
-  { side: "left",  text: "hehe, its again me so i basically ordered u......" },
-  { side: "left",  text: "okayy.. ab post 2 pe jao" },
+  { side: "left",  text: "hehe, its me again......\nI know i can't feel period cramps myself...but i hate it that u have to go through it every monthh..\nSO i ordered u EVA HEATING PAD i hope it makes u feel a little more comfortable❤️\nI know abhii unclee haii😭, i didn't knew jab order kiyaa thaa..it will get delivered aj 1 jan ko hiii i will get it placed at the usual spot fir text ya call u...you can take it whenever you feel comfortable" },
+  { side: "left",  text: "okayy..waise password for post2 is - us4life" },
 
   // PAGE 4
-  { side: "left",  text: "its over noww..." },
-  { side: "left",  text: "happy new year to u my love, and happy new year to us!!" }
+  { side: "left",  text: "The website is over noww, but we're just starting" },
+  { side: "left",  text: "Happy new year, my love❤️ and Happy new year to us!!" }
 ];
 
 
