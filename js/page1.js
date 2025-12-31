d1891f18244127ec8e687e167233e5af515fb0d7
Yash-debug489 <yash.kumaranime@gmail.com>
2025-12-31 14:20:28 +0530

    Update page1.js

--- js/page1.js
+++ js/page1.js
@@ -1,20 +1,3 @@
-const qr = document.getElementById("quickReplyOverlay");
-
-// watch chat for "Wanna see it??"
-const observer = new MutationObserver(() => {
-  if (chat.innerText.includes("Wanna see it??")) {
-    qr.style.display = "flex";
-    observer.disconnect();
-  }
-});
-
-observer.observe(chat, { childList: true, subtree: true });
-
-// hide replies on click
-qr.onclick = () => {
-  qr.style.display = "none";
-};
-
 
 
 disableProfileClick();
